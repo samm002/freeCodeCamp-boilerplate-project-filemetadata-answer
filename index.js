9e459c477725ada9a3a7b2d78881886177a442e4
Samuel Djodi <samuelsdjodis@gmail.com>
2023-10-03 04:07:42 +0700

    final answer File Metadata Microservice with save file to local and store metadata in database feature

--- index.js
+++ index.js
@@ -1,20 +1,75 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+const mongoose = require("mongoose");
 
 var app = express();
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
+const storage = multer.diskStorage({
+  destination: `${__dirname}/public/img/` ,
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `${file.originalname}.${ext}`);
+  },
+});
+const upload = multer({ storage: storage })
 
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
 
+// Creating Database Schema
+const Schema = mongoose.Schema;
+
+const PictureMetadaSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: Number,
+    required: true,
+  }
+});
+
+const PictureMetadataModel = mongoose.model("picture_metadata", PictureMetadaSchema);
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single('upfile'), async (req, res) => {
+  if(!req.file) {
+    res.json({message: 'No file inserted!'});
+  }
+
+  const metadata = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  }
+
+  const pictureMetadata = new PictureMetadataModel(metadata);
+
+  try {
+    await pictureMetadata.save()
+    res.json(metadata)
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+const host = process.env.HOST;
+app.listen(port, host, function () {
+  console.log(`Your app is listening on http://${host}:${port}`);
 });
